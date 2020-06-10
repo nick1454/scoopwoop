653b975aaebb0f4bd3f9a07db4fff9e7f1e2cfdd
nick1454 <nitin.nick1454@gmail.com>
2020-06-10 22:56:37 +0530

    basic auth setup complete

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
     return view('index');
 });
 Route::get('/about', 'DefaultController@about');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
